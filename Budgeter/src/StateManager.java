178377ef16613de212dceae14ef8347abb75ee17
Martin Huynh <martinhuynhp@gmail.com>
2021-11-14 16:07:30 -0800

    Push project and StateManager class.

---
+++ Budgeter/src/StateManager.java
@@ -0,0 +1,20 @@
+import java.awt.HeadlessException;
+
+import javax.swing.JFrame;
+
+public class StateManager extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 977693761657240777L;
+
+	public StateManager() throws HeadlessException {
+		
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
