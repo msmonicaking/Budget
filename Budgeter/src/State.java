ea57cbc6b6ec63c51bb926409b7e4591814b2bdf
Martin Huynh <martinhuynhp@gmail.com>
2021-11-15 21:13:55 -0800

    Created a State class that should be implemented by every page.

---
+++ Budgeter/src/State.java
@@ -0,0 +1,31 @@
+import java.awt.Component;
+import java.awt.LayoutManager;
+import java.util.LinkedList;
+
+import javax.swing.JPanel;
+
+public class State extends JPanel {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1367216474109123645L;
+	protected static StateManager sm;
+	public State() {
+		
+	}
+
+	protected void switchPanel(JPanel panel) {
+		sm.pane.removeAll();
+		sm.pane.add(panel);
+		sm.pane.repaint();
+		sm.pane.revalidate();
+	}
+
+	
+	protected void centerWidth(Component com) {
+		int x = (getSize().width / 2) - (com.getSize().width / 2);
+		com.setBounds(x, com.getY(), com.getWidth(), com.getHeight());
+	}
+	
+}
