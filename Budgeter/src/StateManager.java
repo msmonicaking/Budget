244afe89bea3cdf9936a4f0d7200f2b2055d1f58
Martin Huynh <martinhuynhp@gmail.com>
2021-11-15 21:19:50 -0800

    Added Login and Menu state. Added switchPanel method.

--- Budgeter/src/StateManager.java
+++ Budgeter/src/StateManager.java
@@ -1,6 +1,11 @@
+import java.awt.Frame;
 import java.awt.HeadlessException;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.JFrame;
+import javax.swing.JLayeredPane;
+import javax.swing.JPanel;
 
 public class StateManager extends JFrame {
 
@@ -8,13 +13,43 @@ public class StateManager extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 977693761657240777L;
+	protected JLayeredPane pane;
+	protected JPanel mainScreen;
+	protected JPanel loginScreen;
+
 
 	public StateManager() throws HeadlessException {
+		super("BudGET");
+		State.sm = this;
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pane = getLayeredPane();
+		loginScreen = new Login();
+		settings();
+		add(loginScreen);
+	}
+
+	protected void initMain() {
+		mainScreen = new Main();
+	}
+	
+	private void settings() {
+		setSize(1280, 720);
+		setVisible(true);
 		
 	}
 
 	public static void main(String[] args) {
+		StateManager sm = new StateManager();
+	}
 
+	/*
+	 * Switches screen to "panel".
+	 */
+	public void switchPanel(JPanel panel) {
+		pane.removeAll();
+		pane.add(panel);
+		pane.repaint();
+		pane.revalidate();
 	}
 
 }
