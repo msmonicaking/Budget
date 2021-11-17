5c46b9685d012fdcc3e446deb4ddefe130ee9870
csumali <zmoly7@gmail.com>
2021-11-16 20:34:55 -0800

    Moved Transaction into project

--- Budgeter/src/FileHandler/Transaction.java
+++ Budgeter/src/FileHandler/Transaction.java
@@ -1,12 +1,47 @@
-package FileHandler;
+// 
+// Transaction.java
+// Monica King
+// November 14, 2021
+//
 
-/* Holds data of one transaction. (category, cost, name)
- * 
- */
 public class Transaction {
+    
+    String category;
+    String name;
+    Double price;
+    Date purchDate;
 
-	public Transaction() {
-		// TODO Auto-generated constructor stub
-	}
+    // Constructor for class Transaction
+    public Transaction(String category, String name, Double price, Date purchDate) {
+
+        // add test to see if category is valid?
+        setCategory(category);
+        this.name = name;
+        // I assume all amounts of price are good, should we change?
+        this.price = price;
+        this.purchDate = purchDate;
+
+    }
+
+    // setter for Category
+    // does not currently test for validity of category
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+//-----------------------------------------------------------------------------
+
+    public String getCategory() {
+        return category;
+    }
+    public String getName() {
+        return name;
+    }
+    public Double getPrice() {
+        return price;
+    }
+    public Date getDate() {
+        return purchDate;
+    }
 
 }
