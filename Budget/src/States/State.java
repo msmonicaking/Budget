1dea6c366d6527d042fe154390cf92225cfb7169
Monica <msmonicaking@gmail.com>
2021-11-18 22:34:48 -0800

    testing a github thing ignore this

--- Budget/src/States/State.java
+++ Budget/src/States/State.java
@@ -10,9 +10,9 @@ public class State extends JPanel {
 
 	// Potential Color pallette for ButGET
 	// see Java Swing google doc for visual aid
-	Color teaGreen = new Color(201, 228, 202);
+	Color teaGreen = new Color(201, 228, 202); // text color?
 	Color etonBlue = new Color(135, 187, 162);
-	Color steelTeal = new Color(85, 130, 139);
+	Color steelTeal = new Color(85, 130, 139); 
 	Color deepSpace = new Color(59, 96, 100);
 	Color charcoal = new Color(54, 73, 88); // background color currently
 
