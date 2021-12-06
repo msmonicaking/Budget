de9b2d0a760bc53b115530cd7031414bdb1e7087
Martin Huynh <martinhuynhp@gmail.com>
2021-12-05 19:04:30 -0800

    Made colors protected so that they could be accessed in a different package.

--- Budget/src/States/State.java
+++ Budget/src/States/State.java
@@ -15,11 +15,11 @@ public class State extends JPanel {
 
 	// Potential Color pallette for ButGET
 	// see Java Swing google doc for visual aid
-	Color teaGreen = new Color(201, 228, 202); // text color?
-	Color etonBlue = new Color(135, 187, 162);
-	Color steelTeal = new Color(85, 130, 139);
-	Color deepSpace = new Color(59, 96, 100);
-	Color charcoal = new Color(54, 73, 88); // background color currently
+	protected Color teaGreen = new Color(201, 228, 202); // text color?
+	protected Color etonBlue = new Color(135, 187, 162);
+	protected Color steelTeal = new Color(85, 130, 139);
+	protected Color deepSpace = new Color(59, 96, 100);
+	protected Color charcoal = new Color(54, 73, 88); // background color currently
 
 	Font headerFont = new Font("Arial", Font.ITALIC, 100);
 
