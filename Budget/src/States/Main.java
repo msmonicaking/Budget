32138322543704c0b8523f0d09e8ff316e74dd11
Martin Huynh <martinhuynhp@gmail.com>
2021-12-07 01:20:41 -0800

    Added antialiasing and made test category boxes.

--- Budget/src/States/Main.java
+++ Budget/src/States/Main.java
@@ -8,8 +8,11 @@ import java.util.ArrayList;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+
 import javax.swing.JComponent;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
@@ -102,6 +105,7 @@ public class Main extends State {
 		 * 
 		 */
 		private static final long serialVersionUID = -6883981624969242952L;
+		private ArrayList<CategoryBox> list = new ArrayList<>();
 		int monthInt;
 
 		public DataPane(int monthInt) {
@@ -116,7 +120,18 @@ public class Main extends State {
 		private void loadCategories() {
 			CategoryBox c1 = new CategoryBox("Food");
 			c1.setBackground(etonBlue);
-			add(c1);
+			CategoryBox c2 = new CategoryBox("Entertainment");
+			c2.setBackground(etonBlue);
+			list.add(c1);
+			list.add(c2);
+			
+			int yOff = 0;
+			int spacing = 10;
+			for (CategoryBox c : list) {
+				c.setOffset(0, yOff);
+				add(c);
+				yOff = c.getHeight() + spacing;
+			}
 		}
 
 		private void init() {
@@ -160,8 +175,6 @@ public class Main extends State {
 			private ArrayList<Transaction> list = new ArrayList<>();
 			private static final long serialVersionUID = -8324410991043185795L;
 
-			private int categorySize = 30;
-
 			public CategoryBox(String category) {
 				this.category = category;
 				loadTemp();
@@ -169,8 +182,8 @@ public class Main extends State {
 			}
 
 			public void init() {
-				setLocation(10, 125);
-				setSize(500, 100);
+				setLocation(10, 150);
+				setSize(800, 200);
 				addMouseListener(new MouseAdapter() {
 					@Override
 					public void mouseClicked(MouseEvent e) {
@@ -178,6 +191,10 @@ public class Main extends State {
 						System.out.println("clicked");
 					}
 				});
+			}
+			
+			public void setOffset(int xOff, int yOff) {
+				setLocation(getX() + xOff, getY() + yOff);
 			}
 
 			private void loadTemp() {
@@ -191,24 +208,54 @@ public class Main extends State {
 
 			@Override
 			protected void paintComponent(Graphics g) {
-				drawBox((Graphics2D) g);
-				drawHeader((Graphics2D) g);
+				Graphics2D g2 = (Graphics2D) g;
+				g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+				drawBox(g2);
+				drawHeader(g2);
+				drawItems(g2);
 			}
 
 			void drawBox(Graphics2D g) {
 				if (isVisible()) {
 					g.setColor(getBackground());
 					g.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
-					Font categoryFont = new Font("Arial", 0, categorySize);
+					Font categoryFont = new Font("Arial", 0, 50);
 					g.setColor(Color.WHITE);
 					g.setFont(categoryFont);
-					g.drawString(category, 5, 30);
+					g.drawString(category, 10, 45);
 				}
 			}
 
 			void drawHeader(Graphics2D g) {
+				int xOff = 10;
+				int yOff = 75;
 				if (isVisible()) {
 					g.setColor(Color.WHITE);
+					Font font = new Font("Arial", 0, 25);
+					g.setFont(font);
+					g.drawString("Day", xOff + 10, yOff);
+					g.drawString("Name", xOff + 90, yOff);
+					g.drawString("Cost", xOff + getWidth() - 130, yOff);
+				}
+			}
+			
+			void drawItems(Graphics2D g) {
+				int xOff = 10;
+				int yOff = 110;
+				int spacing = 5;
+				
+				g.setColor(Color.WHITE);
+				Font font = new Font("Arial", 0, 25);
+				g.setFont(font);
+				FontMetrics fm = g.getFontMetrics();
+				
+				int fontHeight = fm.getMaxAscent();
+				spacing += fontHeight;
+				
+				for (int i = 0; i < 2 && list.size() >= 2; i++) {
+					g.drawString(list.get(i).getDate().day + "", xOff + 10, spacing * i + yOff);
+					g.drawString(list.get(i).getName(), xOff + 90, spacing * i + yOff);
+					g.drawString(list.get(i).getPrice() + "", xOff + getWidth() - 130, spacing * i + yOff);
 				}
 			}
 
